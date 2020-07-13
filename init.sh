d87094429d1c34ddd61200a7635350fe543835ae
Anar <genzonline@yandex.ru>
2020-07-13 13:31:57 +0300

    fixes

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
 sudo ln -sf /home/anar/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
