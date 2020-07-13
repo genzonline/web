5a6c4ceb3d092fbbcee7eb62ff7444613bef0b3a
Anar <genzonline@yandex.ru>
2020-07-13 14:19:31 +0300

    fix1000

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sf /home/anar/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ && sudo mv nginx.conf default
 sudo /etc/init.d/nginx restart
