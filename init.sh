565589b3495b73c7526d1ac8f338d82781843345
Anar <genzonline@yandex.ru>
2020-07-15 19:02:00 +0300

    stepik:2.1, attempt: 1

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf /home/anar/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -c /home/anar/box/web/etc/gunicorn.conf.py hello:wsgi_application
+cd ask
+gunicorn -c /home/anar/box/web/etc/gunicorn-django.conf.py ask.wsgi
+# gunicorn -c /home/anar/box/web/etc/gunicorn.conf.py hello:wsgi_application
