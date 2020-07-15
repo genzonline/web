c6a059e274207f584947d4fe01bd02eee644cc29
Anar <genzonline@yandex.ru>
2020-07-15 18:04:13 +0300

    Final version of 1.Static site

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo cp /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ && sudo mv nginx.conf default
+sudo ln -sf /home/anar/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -c /home/anar/box/web/etc/gunicorn.conf.py hello:wsgi_application
+
+
+#sudo ln -sf /home/anar/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
