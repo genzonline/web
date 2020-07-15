4c691621a718a054fca959872ce21ba3432e9495
Anar <genzonline@yandex.ru>
2020-07-15 19:50:29 +0300

    attempt 2

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include, url
-from django.urls import path
 
 urlpatterns = [
 	url(r'^', include('qa.urls')),
