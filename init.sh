1d8f3614f2211267509821e12071d39ea0603593
Anar <64272615+genzonline@users.noreply.github.com>
2020-07-15 17:53:34 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ && sudo mv nginx.conf default
 sudo /etc/init.d/nginx restart
+gunicorn -c /home/anar/box/web/etc/gunicorn.conf.py hello:wsgi_application
