153686823359b02666c9a1a2c7f31f4abfb79922
Anar <genzonline@yandex.ru>
2020-07-18 01:13:27 +0300

    2.6 v.1 fixed db.sh

--- ask/db.sh
+++ ask/db.sh
@@ -1,5 +1,5 @@
 sudo /etc/init.d/mysql restart
-sudo mysql -uroot -e "create database myproject;"
+sudo mysql -uroot -e "CREATE DATABASE stepic_web;"
 sudo mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'password';"
-sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'box'@'localhost';"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost';"
 sudo mysql -uroot -e "FLUSH PRIVILEGES;"
