7fa220425f951bf0f543162a9fceb2eeaca0289e
Anar <genzonline@yandex.ru>
2020-07-17 14:08:41 +0300

    2.3 v.2

--- ask/db.sh
+++ ask/db.sh
@@ -1,5 +1,5 @@
 sudo /etc/init.d/mysql restart
-mysql -uroot -e "create database myproject;"
-mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'password';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'box'@'localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
+sudo mysql -uroot -e "create database myproject;"
+sudo mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'password';"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'box'@'localhost';"
+sudo mysql -uroot -e "FLUSH PRIVILEGES;"
